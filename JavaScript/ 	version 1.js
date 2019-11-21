a1362322879d37d6c9a4a2d2f40b2639f55a9b72
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-11-21 13:00:03 +0100

    Add version 1 file

---
+++ JavaScript/ 	version 1.js
@@ -0,0 +1,22 @@
+
+            //store todos
+
+var todos = ['item 1', 'item 2', 'item 3']
+
+//Display todos
+console.log('My Todos:', todos)
+
+//add new todos
+todos.push('item 4')
+todos.push('item 5')
+
+//change item ---
+todos[0] = 'item 1 updated'
+
+//delete item ---
+todos.splice(0, 1)
+todos.splice(3, 1)
+              
+
+            
+            
