de3aa5f9f46dbb80b763fbc623833b1e610b97f2
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-11-22 15:05:15 +0100

    add new feature

--- JavaScript/version 3.js
+++ JavaScript/version 3.js
@@ -4,4 +4,27 @@ var myComputer = {
     operatingSystem: "mac",
     screenSize: "15 inches",
     purchaseYear: 2011
-  };
+  }
+
+  var mohanad = {
+    name: 'Mohanad',
+    sayName: function() {
+      console.log(this.name);
+    }  
+  }
+  // todos array on an object
+    var todoList = {
+      todos: ['item 1', 'item 2', 'item 3']
+   }
+     console.log(todoList);
+
+   
+   // display todos method
+    var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+   _displayTodos: function () {
+        console.log('My Todos', this.todos);
+      }
+   }
+
+
